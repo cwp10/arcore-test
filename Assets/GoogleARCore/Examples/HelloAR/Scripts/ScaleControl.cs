e875d845a07fe306f105c993c41c0d39242b1dac
WonPyoung Choi <cwp10@msn.com>
2018-08-28 17:35:32 +0900

    UI 화면에서 스케일을 조절 할 수 있는 컨트롤러 제작

---
+++ Assets/GoogleARCore/Examples/HelloAR/Scripts/ScaleControl.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScaleControl : MonoBehaviour 
+{
+	[SerializeField] private Text scaleText_ = null;
+
+	private Transform _model = null;
+
+	public void SetTargetModel(Transform model)
+	{
+		_model = model;
+	}
+
+	public void OnValueChanged(float value)
+	{
+		if(_model == null)
+		{
+			return;
+		}
+		_model.localScale = Vector3.one * value;
+		scaleText_.text = string.Format("{0:0.000}", value);
+	}
+}
---
+++ Assets/GoogleARCore/Examples/HelloAR/Scripts/UlsanChunji.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UlsanChunji : MonoBehaviour 
+{
+	private void OnEnable()
+	{
+		ScaleControl scaleControl = GameObject.Find("ScaleControl").GetComponent<ScaleControl>();
+		scaleControl.SetTargetModel(this.transform);
+	}
+}
