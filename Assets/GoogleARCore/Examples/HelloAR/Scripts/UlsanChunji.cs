a4a56193151fcf8cab0d7d761b8669cc6afc50c3
WonPyoung Choi <cwp10@msn.com>
2018-08-29 15:42:43 +0900

    탑 모델 생성시 기본 스케일값으로 셋팅

--- Assets/GoogleARCore/Examples/HelloAR/Scripts/UlsanChunji.cs
+++ Assets/GoogleARCore/Examples/HelloAR/Scripts/UlsanChunji.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 
 public class UlsanChunji : MonoBehaviour 
 {
+	private const float kScale = 0.2f;
 	private void OnEnable()
 	{
+		this.transform.localScale = Vector3.one * kScale;
 		ScaleControl scaleControl = GameObject.Find("ScaleControl").GetComponent<ScaleControl>();
 		scaleControl.SetTargetModel(this.transform);
+		scaleControl.OnValueChanged(kScale);
 	}
 }
